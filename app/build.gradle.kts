6ec8a298de4420db78c5bc4946f1dc0560666794
omi-72 <omi@techetronventures.com>
2023-09-22 17:44:17 +0600

    This a Project for Brain Station Test

---
+++ app/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-kapt")
+    id ("kotlin-parcelize")
+    id ("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.bstest23"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.bstest23"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation ("androidx.core:core-ktx:1.10.1")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.11.0-alpha01")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation ("androidx.biometric:biometric-ktx:1.2.0-alpha05")
+    implementation ("com.google.firebase:firebase-analytics:21.3.0")
+    implementation ("com.google.firebase:firebase-crashlytics:18.4.0")
+    implementation ("com.google.firebase:firebase-messaging:23.2.1")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+
+    //google truth for tdd
+    implementation ("com.google.truth:truth:1.1.5")
+    androidTestImplementation ("com.google.truth:truth:1.1.5")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    //coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("androidx.activity:activity-ktx:1.7.2")
+
+    //live data
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+
+    //Room Database
+    implementation ("androidx.room:room-runtime:2.5.2")
+    kapt ("androidx.room:room-compiler:2.5.2")
+    implementation ("androidx.room:room-ktx:2.5.2")
+    androidTestImplementation ("androidx.room:room-testing:2.5.2")
+
+    //glide for image
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
+    //Otp View
+    implementation ("com.github.aabhasr1:OtpView:v1.1.2")
+}
---
+++ app/src/main/java/com/example/bstest23/networkCommunication/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.example.bstest23.networkCommunication
+
+class ApiConstants {
+    companion object {
+        const val BASE_URL: String = "https://dummyjson.com"
+        const val API_PRODUCTS: String = "$BASE_URL/products"
+    }
+}
